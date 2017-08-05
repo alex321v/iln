9a1fc8e68598434df8d1cc3c3c60c298891dd50f
Roberto Rossetti <gx@BomboAir.local>
2017-08-05 16:23:29 +0200

    username null

--- TelegramIlnBot.java
+++ TelegramIlnBot.java
@@ -1,6 +1,5 @@
 package com.legrand.iln;
 
-
 /**
  * Progetto ILN
  * Copyright (C) 2003-2017 Monsieur Legrand
@@ -39,209 +38,212 @@ import org.telegram.telegrambots.exceptions.*;
 import org.telegram.telegrambots.updateshandlers.*;
 
 public class TelegramIlnBot extends TelegramLongPollingBot {
-	
+
 	String nickName;
-     	boolean debug = false;
-     	String motd = new String();
-     	Analyzer analisi;
-     	String tipoResponse;
-     	String visitor;
+	boolean debug = false;
+	String motd = new String();
+	Analyzer analisi;
+	String tipoResponse;
+	String visitor;
 	LogServer log;
 
 	Hashtable learningTable = new Hashtable();
 
-      /**
-      * Costruttore. Viene invocato automaticamente alla partenza del programma.
-      * Non c'e' bisogno di effettuare chiamate esplicite.
-      *
-      * @param aNick nickname dell'istanza del programma
-      * @param deb   un booleano che indica il livello di debug: 
-      *              true = debug alto, false = debug basso.
-      */
-     public TelegramIlnBot(String aNick, boolean deb, LogServer alog){
-	nickName = aNick;
-	debug = deb;
-	log = alog;
-	analisi = new Analyzer(nickName, debug);
-     }
+	/**
+	 * Costruttore. Viene invocato automaticamente alla partenza del programma.
+	 * Non c'e' bisogno di effettuare chiamate esplicite.
+	 *
+	 * @param aNick
+	 *            nickname dell'istanza del programma
+	 * @param deb
+	 *            un booleano che indica il livello di debug: true = debug alto,
+	 *            false = debug basso.
+	 */
+	public TelegramIlnBot(String aNick, boolean deb, LogServer alog) {
+		nickName = aNick;
+		debug = deb;
+		log = alog;
+		analisi = new Analyzer(nickName, debug);
+	}
 
-	 @Override
-	 public void onUpdateReceived(Update update) {
+	@Override
+	public void onUpdateReceived(Update update) {
 		String line = null;
-            	String prefix, comando, params;
-            	String[] response;
+		String prefix, comando, params;
+		String[] response;
 
-	        // We check if the update has a message and the message has text
-    		if (update.hasMessage() && update.getMessage().hasText()) {
-        		// Set variables
+		// We check if the update has a message and the message has text
+		if (update.hasMessage() && update.getMessage().hasText()) {
+			// Set variables
 			long chat_id = update.getMessage().getChatId();
 			prefix = update.getMessage().getFrom().getUserName();
+			if(prefix==null) {
+				prefix = update.getMessage().getFrom().getFirstName();
+			}
 			if (debug) {
 				log.logga("prefix = " + prefix);
 			}
-	       		params = nickName + " :" + update.getMessage().getText().toLowerCase().trim();
+			params = nickName + " :" + update.getMessage().getText().toLowerCase().trim();
 			if (debug) {
 				log.logga("params = " + params);
 			}
 			log.logga(prefix + ": " + params);
 
-	       		response = trattaPrivMsg(prefix, params);
-        		
-        		
-			for (int i=0; i<response.length; i++) {
+			response = trattaPrivMsg(prefix, params);
+
+			for (int i = 0; i < response.length; i++) {
 				log.logga("Risposta " + i + ": " + response[i]);
- 		       		SendMessage message = new SendMessage() // Create a message object object
-                			      		.setChatId(chat_id)
-                			      		.setText(response[i]);
-        			try {
-            				sendMessage(message); // Sending our message object to user
-        			} catch (TelegramApiException e) {
-            				e.printStackTrace();
-        			}
+				SendMessage message = new SendMessage() // Create a message
+														// object object
+						.setChatId(chat_id).setText(response[i]);
+				try {
+					sendMessage(message); // Sending our message object to user
+				} catch (TelegramApiException e) {
+					e.printStackTrace();
+				}
 			}
-    		}
-    	 }
-				
-         @Override
-         public String getBotUsername() {
-	    return nickName;
-         }
-				
-         @Override
-         public String getBotToken() {
-           return "405806016:AAF-QgVcjcijshxn2USVGrioV1y5lKjijng";
-	 }
+		}
+	}
 
-	      /**
-       * La documentazione di questa funzione e' la stessa della funzione omonima in @see IrcClient
-       */
-        public String[] trattaPrivMsg(String prefix, String params) {
-            StringTokenizer tok = new StringTokenizer(params, " ");
-	    String k = tok.nextToken();
-	    String[] response = new String[3];
+	@Override
+	public String getBotUsername() {
+		return nickName;
+	}
 
-	    int nRes = 1;
-	    boolean command = false;
-
-	    if (k.equals(nickName)) {
-	    	String toNick = prefix;
-	    	if (learningTable.containsKey(toNick))
-	    	   if (((String)learningTable.get(toNick)).equals("begin_learn"))
-	    	      response = learn2(toNick, params);
-	    	   else
-	    	      response = learnEnd(toNick, params);
-	    	else {
-	    	    String k2 = tok.nextToken();
-	    	    if (k2.equals(":versione")){
-	    		response = analisi.version();
-	    		return response;
-	            }
-	            else if (k2.trim().equals(":chi sei?")){
-	        	response = analisi.chisei();
-	        	return response;
-	            }
-	            else if (k2.substring(1).trim().equals(nickName) || 
-	                     k2.substring(1).trim().equals(nickName.toLowerCase())){
-			response[0] = "Sì?";
-	                return response;
-	       	    }
-	       	    else if (k2.equals(":stop")){
-	       		System.out.println("Ordine di arresto programma. Ciao a presto");
-	       		System.out.println("Mi fermo qui. Ciao a tutti");
-	       		System.out.println("QUIT");
-	       		System.exit(0);
-	       	    }
-	            else {
-	       	        response = analisi.generation(toNick, params, 1);
-	       	        if (response != null && response[0] !=  "learn"){
-	       	           if (response[0].indexOf("CMD") > -1){
-	       	           	 //log.logga("command = true");
-				 nRes =  2;
-	       	                 response = analisi.esegueComando(response[0], params, nRes);
-	       	                 command = true;
-	       	           }
-	       	           if (!response[0].equals("learn")){
-	       	              return response;
-	       	              }
-	       	           if (command){
-	       	              	nRes = 1;
-	       	              	command = false;
-				//log.logga("command = false");
-	       	              }
-	       	           }
-	       	           else {
-			       log.logga("Entrato nell'else del learn");
-	       	     	       learningTable.put(toNick, "begin_learn");
-	       	     	       response[0] = toNick + " Non so cosa significhi. La parola chiave quale è?";
-			       log.logga("Settato " + response[0]);
-			       return response;
-	                   }
-	       	         }
-	       	    }
-	       }
-	   return response; 
+	@Override
+	public String getBotToken() {
+		//return "405806016:AAF-QgVcjcijshxn2USVGrioV1y5lKjijng"; //gattino
+		return "448239661:AAFulFHqoaqw0HR6PC_lQxMyY3sXOYWSEhk"; //topino
 	}
 
 	/**
-	 * La documentazione di questa funzione e' la stessa di quella di @see IrcClient
+	 * La documentazione di questa funzione e' la stessa della funzione omonima
+	 * in @see IrcClient
 	 */
-        private String trovaNick(String stringa){
-         StringBuffer rispostina = new StringBuffer();
-         int i = 1;
-         while (stringa.charAt(i) != '!'){
-            rispostina.append(stringa.charAt(i));
-            i++;
-            }
+	public String[] trattaPrivMsg(String prefix, String params) {
+		StringTokenizer tok = new StringTokenizer(params, " ");
+		String k = tok.nextToken();
+		String[] response = new String[3];
 
-         return rispostina.toString();
+		int nRes = 1;
+		boolean command = false;
 
-        }
+		if (k.equals(nickName)) {
+			String toNick = prefix;
+			if (learningTable.containsKey(toNick))
+				if (((String) learningTable.get(toNick)).equals("begin_learn"))
+					response = learn2(toNick, params);
+				else
+					response = learnEnd(toNick, params);
+			else {
+				String k2 = tok.nextToken();
+				if (k2.equals(":versione")) {
+					response = analisi.version();
+					return response;
+				} else if (k2.trim().equals(":chi sei?")) {
+					response = analisi.chisei();
+					return response;
+				} else if (k2.substring(1).trim().equals(nickName)
+						|| k2.substring(1).trim().equals(nickName.toLowerCase())) {
+					response[0] = "Sì?";
+					return response;
+				} else if (k2.equals(":stop")) {
+					System.out.println("Ordine di arresto programma. Ciao a presto");
+					System.out.println("Mi fermo qui. Ciao a tutti");
+					System.out.println("QUIT");
+					System.exit(0);
+				} else {
+					response = analisi.generation(toNick, params, 1);
+					if (response != null && response[0] != "learn") {
+						if (response[0].indexOf("CMD") > -1) {
+							// log.logga("command = true");
+							nRes = 2;
+							response = analisi.esegueComando(response[0], params, nRes);
+							command = true;
+						}
+						if (!response[0].equals("learn")) {
+							return response;
+						}
+						if (command) {
+							nRes = 1;
+							command = false;
+							// log.logga("command = false");
+						}
+					} else {
+						log.logga("Entrato nell'else del learn");
+						learningTable.put(toNick, "begin_learn");
+						response[0] = toNick + " Non so cosa significhi. La parola chiave quale è?";
+						log.logga("Settato " + response[0]);
+						return response;
+					}
+				}
+			}
+		}
+		return response;
+	}
 
 	/**
-	 * La documentazione di questa funzione e' la stessa di quella di @see IrcClient
+	 * La documentazione di questa funzione e' la stessa di quella di @see
+	 * IrcClient
 	 */
-        private String[] learn2(String toNick, String params) {
-	   String[] response = new String[3];
-           int k = params.indexOf(":");
-           String newKeyWord   = params.substring(k+1).trim();
-	   log.logga ("Siamo in leanr2");
-           if (newKeyWord.indexOf(nickName) >-1 ){ //okkio! Stanno tentando l'hack del nome!
-	        response[0] = "No no non puoi darmi come chiave qualcosa con il mio nome...";
-		response[1] = "poi va a finire che quando la gente mi invoca io rispondo così";
-		response[2] = "e mica ci casco... sce'!";
+	private String trovaNick(String stringa) {
+		StringBuffer rispostina = new StringBuffer();
+		int i = 1;
+		while (stringa.charAt(i) != '!') {
+			rispostina.append(stringa.charAt(i));
+			i++;
+		}
+
+		return rispostina.toString();
+
+	}
+
+	/**
+	 * La documentazione di questa funzione e' la stessa di quella di @see
+	 * IrcClient
+	 */
+	private String[] learn2(String toNick, String params) {
+		String[] response = new String[3];
+		int k = params.indexOf(":");
+		String newKeyWord = params.substring(k + 1).trim();
+		log.logga("Siamo in leanr2");
+		if (newKeyWord.indexOf(nickName) > -1) { // okkio! Stanno tentando
+													// l'hack del nome!
+			response[0] = "No no non puoi darmi come chiave qualcosa con il mio nome...";
+			response[1] = "poi va a finire che quando la gente mi invoca io rispondo così";
+			response[2] = "e mica ci casco... sce'!";
+			learningTable.remove(toNick);
+		} else {
+			log.logga("Siamo nell'else di learn2");
+			learningTable.put(toNick, newKeyWord);
+			response[0] = "Mi dici una bella frase che riguarda ciò? Io la imparo.";
+			response[1] = "Dai, una cosa simpatica...";
+			response[2] = "Fammi migliorare...";
+		}
+		log.logga("response[0] dopo l'else: " + response[0]);
+		return response;
+	}
+
+	/**
+	 * La documentazione di questa funzione e' la stessa di quella di @see
+	 * IrcClient
+	 */
+	private String[] learnEnd(String toNick, String params) {
+		String[] response = new String[2];
+
+		int k = params.indexOf(":");
+		String newPhraseLearned = params.substring(k + 1).trim();
+		String newKeyWord = (String) learningTable.get(toNick);
+		// Controllo bug di gi0
+		if (newKeyWord.equals("") || newPhraseLearned.equals("")) {
+			response[0] = "Ehi mica vorrai fregarmi con qualche stupido hack... sei un lamer :))";
+			response[1] = "Non provarci più!";
+		} else {
+			response[0] = "Ho capito: " + newKeyWord + " " + newPhraseLearned;
+			response[1] = "Grazie davvero. Non si finisce mai di imparare :-)";
+			analisi.learning(newKeyWord, newPhraseLearned);
+		}
 		learningTable.remove(toNick);
-           }
-	   else {
-	      log.logga("Siamo nell'else di learn2");
-	      learningTable.put(toNick, newKeyWord);
-              response[0] = "Mi dici una bella frase che riguarda ciò? Io la imparo.";
-	      response[1] = "Dai, una cosa simpatica...";
-	      response[2] = "Fammi migliorare..."; 
-           }
-	   log.logga("response[0] dopo l'else: " + response[0]);
-	   return response;
+		return response;
 	}
-	   
-	/**
-	 * La documentazione di questa funzione e' la stessa di quella di @see IrcClient
-	 */
-        private String[] learnEnd(String toNick, String params){
-            String[] response = new String[2];
-
-            int k = params.indexOf(":");
-            String newPhraseLearned = params.substring(k+1).trim();
-            String newKeyWord   = (String)learningTable.get(toNick);
-	    //Controllo bug di gi0
-	    if (newKeyWord.equals("") || newPhraseLearned.equals("")){
-		response[0] = "Ehi mica vorrai fregarmi con qualche stupido hack... sei un lamer :))";
-		response[1] = "Non provarci più!";
-	    }
-	    else {
-	    	response[0] = "Ho capito: " + newKeyWord + " " + newPhraseLearned;
-	        response[1] = "Grazie davvero. Non si finisce mai di imparare :-)";
-                analisi.learning(newKeyWord, newPhraseLearned);
-	    }
-            learningTable.remove(toNick);
-	    return response;
-        }
 }
